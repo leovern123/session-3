7634b2d32ad8457844785868592efdf378daffb8
leovern123 <umarcp31@gmail.com>
2025-10-21 14:26:44 +0700

    first commit

---
+++ bin/session_3.dart
@@ -0,0 +1,5 @@
+import 'package:session_3/session_3.dart' as session_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${session_3.calculate()}!');
+}
---
+++ test/session_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:session_3/session_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
